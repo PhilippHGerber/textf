38cd5632ed3a64bd96d356a09038e204f8759893
Philipp H. Gerber <6642798+PhilippHGerber@users.noreply.github.com>
2026-01-10 15:11:10 +0100

    chore(textf): add copyright and license information to the textf.dart file

--- lib/textf.dart
+++ lib/textf.dart
@@ -1,9 +1,12 @@
+// Copyright (c) 2025 Philipp H. Gerber
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 /// A lightweight text widget library for simple inline formatting.
 ///
 /// This library provides the Textf widget which supports basic
 /// markdown-like formatting for text in Flutter applications.
 library;
 
-// Export the widget for public use
 export 'src/widgets/textf.dart';
 export 'src/widgets/textf_options.dart';
