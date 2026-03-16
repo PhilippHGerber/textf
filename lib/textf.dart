f304e688b7b376596ccca3c92c300f3062b1cbe7
Philipp H. Gerber <6642798+PhilippHGerber@users.noreply.github.com>
2026-03-16 13:50:08 +0100

    feat(exports): add export for textf_options_data widget

--- lib/textf.dart
+++ lib/textf.dart
@@ -32,3 +32,4 @@ export 'src/editing/textf_editing_controller.dart';
 export 'src/widgets/textf.dart';
 export 'src/widgets/textf_ext.dart';
 export 'src/widgets/textf_options.dart';
+export 'src/widgets/textf_options_data.dart';
